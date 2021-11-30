b0a3fa629e5353e74b4251b2b9273a56a1057477
eladjmc <eladjmc88@gmail.com>
2021-11-30 13:46:17 +0200

    added some logic for the hover display and implemented the ack button on breakthrough interface guide

--- js/interface.js
+++ js/interface.js
@@ -4,6 +4,7 @@ export default class Interface {
     lineTwoText: document.querySelector(".interface_text_line_2"),
     lineThreeText: document.querySelector(".interface_text_line_3"),
     lineFourText: document.querySelector(".interface_text_line_4"),
+    acknowledgmentButton: document.querySelector(".acknowledged_button"),
   };
   constructor(player, battleSystem) {
     this.player = player;
@@ -26,20 +27,27 @@ export default class Interface {
     const TalentLevelName = this.player.getTalentLevelName();
     this.selectors.lineOneText.innerHTML = "Talent grade: " + TalentLevelName;
     this.selectors.lineTwoText.innerHTML =
-      "Price to upgrade: " + TalentPrice.toFixed(1) + " SpiritStones.";
+    "Price to upgrade: " + TalentPrice.toFixed(1) + " SpiritStones.";
   }
   listeners() {
     this.player.selectors.header.upgradeTalentButton.addEventListener(
       "mouseover",
       () => {
         this.hoverTalentUpgradeButton();
-      },
-      false
-    );
-    this.player.selectors.header.upgradeTalentButton.addEventListener(
-      "mouseout",
-      this.resetInterface(),
-      false
-    );
-  }
-}
+      }
+      );
+      this.player.selectors.header.upgradeTalentButton.addEventListener(
+        "mouseout",()=>{
+          if (this.player.isBreakthroughNeeded()) {
+            return;
+          }
+          this.resetInterface();
+        });
+        this.selectors.acknowledgmentButton.addEventListener("click",()=>{
+          this.selectors.acknowledgmentButton.style.opacity="0";
+          this.selectors.acknowledgmentButton.disabled=true;
+          this.resetInterface();
+        });
+      }
+    }
+    
